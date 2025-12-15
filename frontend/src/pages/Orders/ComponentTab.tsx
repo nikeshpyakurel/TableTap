71ef830ec7bf42c32478f975abf503914b72d0ae
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-12-15 07:10:20 +0545

    Create ComponentTab.tsx

---
+++ frontend/src/pages/Orders/ComponentTab.tsx
@@ -0,0 +1,26 @@
+// import { Button, Group } from "@mantine/core";
+// import React from "react";
+
+// const ComponentTab = () => {
+//   return (
+//     <>
+//       <Group mt={20}>
+//         <Button
+//           variant="transparent"
+//           color="#ff6347"
+//           style={{ borderBottom: "2px solid #ff6347" }}
+//         >
+//           Active Orders
+//         </Button>
+//         <Button variant="transparent" color="#ff6347">
+//           Completed Orders
+//         </Button>
+//         <Button variant="transparent" color="#ff6347">
+//           Cancelled Orders
+//         </Button>
+//       </Group>
+//     </>
+//   );
+// };
+
+// export default ComponentTab;
