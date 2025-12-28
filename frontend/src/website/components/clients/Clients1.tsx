aca15c3bd9eff81e06b6ba60b5a95e96b05c757c
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-12-28 07:46:35 +0545

    Create Clients1.tsx

---
+++ frontend/src/website/components/clients/Clients1.tsx
@@ -0,0 +1,19 @@
+// import Marquee from "react-fast-marquee";
+// import clientData from './clientsdata.json'
+
+// const Clients = () => {
+//     return (
+//         <div className='p-8 text-headingColor sm:pt-16'>
+//             <h1 className='text-3xl text-gray-700 font-poppins sm:text-4xl font-bold py-8 text-center'>Our Clients</h1>
+//             <div className='client-logo'>
+//                 <Marquee pauseOnHover={true} gradient={false} speed={50} style={{cursor:'pointer'}}>
+//                     {clientData.map((client) => (
+//                         <img key={client.id} src={client.image} alt={client.alt} width='130' className='p-4' />
+//                     ))}
+//                 </Marquee>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Clients
