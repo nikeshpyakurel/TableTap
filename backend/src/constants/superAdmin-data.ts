424799f7b66d36c90aa41e8f3e688e9e4fb0e2f2
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2025-12-28 07:47:30 +0545

    Create superAdmin-data.ts

---
+++ backend/src/constants/superAdmin-data.ts
@@ -0,0 +1,7 @@
+export const SuperAdminInfo = {
+    email: "digitalpravidhi2@gmail.com",
+    password: "password",
+    name: "Digital Pravidhi",
+    address: "New Baneshwor",
+    photo: "https://www.digitalpravidhi.com/wp-content/uploads/2023/09/cropped-DPLogo.jpg"
+};
